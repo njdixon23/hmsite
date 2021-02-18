992586ed9111ecc71c41b7240bf61a3d7671d56c
njdixon23 <79178307+njdixon23@users.noreply.github.com>
2021-02-17 21:27:22 -0500

    hmsite_v0.1
    2.17.21

---
+++ hmsite/src/App.vue
@@ -0,0 +1,31 @@
+//HTML
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="G'Day Boysss"/>
+  </div>
+</template>
+
+//JAVASCRIPT
+<script> 
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+//CSS
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
